06fb385df7138daaa29f7ac00e6cf5de22740b82
AlphaBlend <whizkid3000@hotmail.com>
2017-06-03 08:44:05 -0700

    Initial commit

---
+++ src/org/codespeak/windowshopping/WindowShopping.java
@@ -0,0 +1,16 @@
+package org.codespeak.windowshopping;
+
+/**
+ *
+ * @author AlphaBlend
+ */
+public class WindowShopping {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
