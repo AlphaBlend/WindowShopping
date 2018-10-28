dfc31b0f90a5b3425ccfeb44079659f3db5994ba
MisterVector <whizkid3000@hotmail.com>
2018-10-28 10:41:55 -0700

    Added a configuration class

---
+++ src/org/codespeak/windowshopping/Configuration.java
@@ -0,0 +1,14 @@
+package org.codespeak.windowshopping;
+
+/**
+ * A class that specifies various configuration information
+ *
+ * @author Vector
+ */
+public class Configuration {
+
+    public static final String PLUGIN_NAME = "WindowShopping";
+    public static final String PLUGIN_AUTHOR = "Vector";
+    public static final String PLUGIN_VERSION = "0.0.0";
+
+}
