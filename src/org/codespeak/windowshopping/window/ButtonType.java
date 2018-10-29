32a2fc83724aa99e9d3c444c8c2f715cd98adae4
MisterVector <whizkid3000@hotmail.com>
2018-10-28 20:38:33 -0700

    Added an enum listing all button types for windows

---
+++ src/org/codespeak/windowshopping/window/ButtonType.java
@@ -0,0 +1,40 @@
+package org.codespeak.windowshopping.window;
+
+import org.bukkit.Material;
+
+/**
+ * An enum listing several types of buttons for windows
+ *
+ * @author Vector
+ */
+public enum ButtonType {
+
+    PREVIOUS_PAGE(Material.CHEST),
+    NEXT_PAGE(Material.CHEST),
+    PREVIOUS_MENU(Material.ENDER_CHEST),
+    ITEM_AMOUNT_MODIFIER(Material.DISPENSER),
+    ITEM_AMOUNT_MODIFIER_16(Material.DISPENSER),
+    ITEM_AMOUNT_MODIFIER_32(Material.DISPENSER),
+    ITEM_AMOUNT_MODIFIER_64(Material.DISPENSER),
+    MESSAGE_ITEM(Material.PAPER),
+    PLAYER_SHOP_ITEM(null),
+    WINDOW_ITEM(null),
+    INFO_ITEM(null),
+    NONE(null);
+
+    private final Material mat;
+
+    private ButtonType(Material mat) {
+        this.mat = mat;
+    }
+
+    /**
+     * Gets the material associated with this button
+     *
+     * @return
+     */
+    public Material getMaterial() {
+        return mat;
+    }
+
+}
