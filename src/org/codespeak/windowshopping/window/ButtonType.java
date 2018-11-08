93e3816c8befc4d4cea421bbca2640cd859af614
MisterVector <whizkid3000@hotmail.com>
2018-11-08 08:44:01 -0800

    ButtonType changes
    Renamed WINDOW_ITEM to OPEN_WINDOW_ITEM for clarity.
    
    Made some JavaDoc improvements.
    
    Meta data should be applied to the item stack when
    creating it.
    
    Added a method to create an item for opening a player's
    shop or just an info item instead.

--- src/org/codespeak/windowshopping/window/ButtonType.java
+++ src/org/codespeak/windowshopping/window/ButtonType.java
@@ -3,8 +3,10 @@ package org.codespeak.windowshopping.window;
 import java.util.Collections;
 import java.util.List;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
 /**
  * An enum listing several types of buttons for windows
@@ -22,7 +24,7 @@ public enum ButtonType {
     ITEM_AMOUNT_MODIFIER_64,
     MESSAGE_ITEM,
     PLAYER_SHOP_ITEM,
-    WINDOW_ITEM,
+    OPEN_WINDOW_ITEM,
     INFO_ITEM,
     CHANGE_PRICE_ITEM,
     NONE;
@@ -51,13 +53,13 @@ public enum ButtonType {
     }
 
     /**
-     * Builds a window item given its buttontype, material, display
+     * Builds a window item given its button type, material, display
      * name and lore
      *
      * @param type the type of button
-     * @param material
-     * @param displayName
-     * @param lore
+     * @param material the material of the button
+     * @param displayName the display name of the button
+     * @param lore additional text on the button
      * @return a window item of the specified type, material, display
      * name and lore
      */
@@ -73,7 +75,32 @@ public enum ButtonType {
             meta.setLore(lore);
         }
 
+        stack.setItemMeta(meta);
+
         return new WindowItem(stack, type);
+    }
+
+    /**
+     * Creates a player head window item
+     * @param openShop true to open up a player's shop, false otherwise
+     * @param player the player owning the player head
+     * @param lore additional information for the player's head
+     * @return a player head window item
+     */
+    public static WindowItem buildPlayerHeadItem(boolean openShop, OfflinePlayer player, List<String> lore) {
+        ItemStack stack = new ItemStack(Material.PLAYER_HEAD, 1);
+        SkullMeta skull = (SkullMeta) stack.getItemMeta();
+
+        skull.setDisplayName(player.getName());
+        skull.setOwningPlayer(player);
+
+        if (!lore.isEmpty()) {
+            skull.setLore(lore);
+        }
+
+        stack.setItemMeta(skull);
+
+        return new WindowItem(stack, openShop ? ButtonType.PLAYER_SHOP_ITEM : ButtonType.INFO_ITEM);
     }
 
 }
